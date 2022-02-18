c59338cb4e260bb937a84ca7cfcdc06af4c85add
Catalin Vasile <contact@catalinvasile.dev>
2022-02-18 16:06:45 +0100

    Updated readme, added credentials as part of the example and added a method that automatically sets the sinch app callback url with every docker container restart

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 
-const express = require('express');
-const ngrok = require('ngrok');
+import fetch from 'node-fetch';
+import express from 'express';
+import ngrok from 'ngrok';
+
+const applicationKey = '<APPLICATION_KEY>';
+const applicationSecret = '<APPLICATION_SECRET>';
 
 const PORT = 3000;
 
@@ -9,39 +13,49 @@ const app = express();
 app.use(express.json());
 
 app.post('/incoming-call', async (req, res) => {
-  let requestBody = req.body;
-  let eventName = req.body.event;
-  let svamlResponse;
-  console.log(requestBody);
-  switch (eventName) {
-    case 'ice':
-      svamlResponse = {
-      instructions: [
-          {
-            name: "say",
-            text: "Hi, thank you for calling your Sinch number. Congratulations! You just responded to a phone call.",
-            locale: "en-US"
-          },
-        ],
-      action: {
-          name: "hangup"
-        }
-      };
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(svamlResponse));
-      break;
-    case 'dice':
-      res.sendStatus(200);
-      break;
-    default:
-      console.log("Sorry, something went wrong.")
-      res.sendStatus(404)
-  }
+  let svamlResponse = {
+    instructions: [
+      {
+        name: "say",
+        text: "Hi, thank you for calling your Sinch number. Congratulations! You just responded to a phone call.",
+        locale: "en-US"
+      }
+    ],
+    action: {
+      name: "hangup"
+    }
+  };
+
+  res.json(svamlResponse);
 });
 
 app.listen(PORT, async () => {
   const url = await ngrok.connect(PORT);
   console.log(`Node.js local server is publicly-accessible at ${url}/incoming-call`);
-  console.log(`Please update your Callback URL in the dashboard at https://dashboard.sinch.com/voice/apps`);
+  await updateUrl(url + '/incoming-call');
+  console.log("Your callbackURL has been updated on your dashboard.")
   console.log(`Listening at http://localhost:` + PORT);
 });
+
+async function updateUrl(callbackUrl) {
+  const resp = await fetch(
+    `https://callingapi.sinch.com/v1/configuration/callbacks/applications/${applicationKey}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + Buffer.from(`${applicationKey}:${applicationSecret}`).toString('base64')
+      },
+      body: {
+        url: [
+          {
+            primary: callbackUrl
+          }
+        ]
+      }
+    }
+  );
+
+  const data = await resp.json();
+  console.log(data);
+}
